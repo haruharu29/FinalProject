1b229959d62941a6baed4e9bf77773f93caf3604
haruharu29 <156629469+haruharu29@users.noreply.github.com>
2024-04-09 02:10:16 +0900

    Lost&Found, Student Resources and few modification in other files

--- app/src/main/java/Model/Notification.kt
+++ app/src/main/java/Model/Notification.kt
@@ -1,15 +1,16 @@
 package Model
 
 import java.util.Date
-
 data class Notification(
-    val id: String,
-    val postId: String,
+    val id: String? = null,
+    val postId: String = "",
     val commentId: String? = null,
-    val senderId: String,
-    val senderName: String,
-    val type: String, // "postReply" or "commentReply"
-    val contentPreview: String,
-    var date: Date,
-    val read: Boolean = false
+    val senderId: String = "",
+    val senderName: String = "",
+    val type: String = "", // "postReply" or "commentReply"
+    val contentPreview: String = "",
+    var date: Date = Date(),
+    val read: Boolean = false,
+    val exists: Boolean = true
+
 )
